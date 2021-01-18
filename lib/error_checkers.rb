2cf08a28729cdbd9d1cedaafcd99ae27c5631ccc
carloso0114 <carlossospinag@gmail.com>
2021-01-18 14:21:10 -0500

    add 1 check and test it

--- lib/error_checkers.rb
+++ lib/error_checkers.rb
@@ -2,8 +2,8 @@ class Test
     attr_reader :pattern, :message
   
     def initialize
-      @pattern = /.test2/
-      @message = 'its there'
+      @pattern = /\. /
+      @message = 'There should not be an empty space after calling a class name'
     end
   end
   
