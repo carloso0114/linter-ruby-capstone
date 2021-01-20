2754bf8fd6907eda44c07892dc388816a3847eb6
Carlos Ospina <carlossospinag@gmail.com>
2021-01-20 12:24:49 -0500

    Revert "add Test3 and Test4"
    This reverts commit 04dfe5a40ccfeb0ce49c2cf9c399068f5df280c1.

--- lib/error_checkers.rb
+++ lib/error_checkers.rb
@@ -1,4 +1,4 @@
-class Test1
+class Test
     attr_reader :pattern, :message
   
     def initialize
@@ -6,32 +6,4 @@ class Test1
       @message = 'There should not be an empty space after calling a class name'
     end
   end
-
-  class Test2
-    attr_reader :pattern, :message
-  
-    def initialize
-      @pattern = /\# /
-      @message = 'There should not be an empty space after calling an ID name'
-    end
-  end
-
-  class Test3
-    attr_reader :pattern, :message
-  
-    def initialize
-      @pattern = /\.[a-zA-Z0-9-]+[\{]/
-      @message = 'There should be an empty space before the opening bracket'
-    end
-  end
-
-  class Test4
-    attr_reader :pattern, :message
-  
-    def initialize
-      @pattern = /\#[a-zA-Z0-9-]+[\{]/
-      @message = 'There should be an empty space before the opening bracket'
-    end
-  end
-
   
