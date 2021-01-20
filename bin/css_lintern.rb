b357b127021ba33df35ffdcfcee715d427f9856a
carloso0114 <carlossospinag@gmail.com>
2021-01-20 10:51:07 -0500

    update css_lintern doesnt need require checkers.rb

--- bin/css_lintern.rb
+++ bin/css_lintern.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
 
 require_relative '../lib/linter_logic'
-require_relative '../lib/error_checkers'
 
-# Select the file you want to test here. By default the file is lib/test.md.
-# Copy or move your markdown file into the lib/ sub-directory of this program's root directory
+# Select the file you want to test here. By default the file is lib/style.css
+# Copy or move your CSS file into the lib/ sub-directory of this program's root directory
 
 file = 'lib/style.css'
 
